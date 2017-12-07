ecaf064145c4870507e8149ca9dda35990d48708
Matthieu Fournier <mat.fournier85@gmail.com>
2017-12-07 17:45:04 +0100

    navigation to the wikiPage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { WikiPage } from '../wiki/wiki';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  wikiPage = WikiPage;
 
   constructor(public navCtrl: NavController) {
 
