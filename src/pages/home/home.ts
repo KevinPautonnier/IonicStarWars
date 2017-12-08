37564523aa6a99ac8e8f8a2c1d293ff28c46b960
Killy85 <goureau.n.a@hotmail.fr>
2017-12-08 17:48:02 +0100

    Addind speech bubble for bb8

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,9 +8,27 @@ import { WikiPage } from '../wiki/wiki';
 })
 export class HomePage {
   wikiPage = WikiPage;
+  speechs=[
+    "Click on the planet/starships to view infos about episodes",
+    "Click on the X-Wing to access a list of all datas available",
+    "Click on me to get infos about the association!",
+  ];
+
+  i =0;
 
   constructor(public navCtrl: NavController) {
+    this.bb8speech(this.speechs.length, this.speechs, this);
+  };
 
-  }
+  bb8speech(i, speechs, fun){
+    if(i>0){
+      setTimeout(function(){
+          console.log(i);
+          document.getElementById("speech").innerHTML=speechs[i-1];
+          fun.bb8speech((i-1), speechs, fun);
+      }, 2000);
+    }
+  };
+
 
 }
