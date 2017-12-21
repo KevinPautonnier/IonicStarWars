6d2a446d2400e3d5a4f703b2f2939c2af4d1c14d
Matthieu Fournier <mat.fournier85@gmail.com>
2017-12-21 17:55:15 +0100

    corection lazy load + navigation

--- src/pages/wiki-details/wiki-details.ts
+++ src/pages/wiki-details/wiki-details.ts
@@ -74,6 +74,20 @@ export class WikiDetailsPage {
 				newDetail = { title: prop, value : objValue, isList : false, isUrl : false };
 				if(objValue.slice(0,4) == "http"){
 					newDetail["isUrl"] = true;
+					this.api.getData(objValue, response => {
+						if(response != "404"){
+							for(var obj in this.listDetails){
+								if(this.listDetails[obj].value == objValue){
+									this.listDetails[obj].urlTitle = response[response.principaleAttributeName];
+									console.log("link( " + response[response.principaleAttributeName] + "):" + objValue);
+									break;
+								}
+							}
+						}else{
+							this.listDetailsDetails[prop][obj].urlTitle = "No Data"
+							this.listDetails[prop][obj].objValue = "";
+						}
+					});
 				}
 				break;
 
@@ -86,8 +100,9 @@ export class WikiDetailsPage {
 					//console.log("generateVisualOf:" + objValue[prop2] );
 					var newDetail2 = this.generateVisual(this.api.getDataFromUrl(objValue[prop2]).categorie, objValue[prop2])
 					newDetail2["url"] = objValue[prop2];
+					this.setUrlTitle(objValue[prop2], prop);
 					this.listDetailsDetails[prop].push(newDetail2);
-					console.log("this.[...].push(" + JSON.stringify(newDetail2));
+					//console.log("this.[...].push(" + JSON.stringify(newDetail2));
 					//console.log("object_objValue:" + JSON.stringify(newDetail2.value));
 				}
 				break;
@@ -99,10 +114,29 @@ export class WikiDetailsPage {
 	}
 
 	redirectTo(url){
-		this.modal.showModal();
-		console.log("redirectTo:" + url);
-		this.navigation["categorie"] = this.api.getDataFromUrl(url).categorie;
-		this.navigation["elementId"] = this.api.getDataFromUrl(url).elementId;
-		this.storage.set("navigation", this.navigation).then(navigation => {this.ionViewDidSet();});		
+		if(url.length > 0){	
+			this.modal.showModal();
+			console.log("redirectTo:" + url);
+			this.navigation["categorie"] = this.api.getDataFromUrl(url).categorie;
+			this.navigation["elementId"] = this.api.getDataFromUrl(url).elementId;
+			this.storage.set("navigation", this.navigation).then(navigation => {this.ionViewDidSet();});	
+		}	
+	}
+
+	setUrlTitle(url, title){
+		console.log("setUrlTitle(" + url + ", " + title +" )");
+		this.api.getData(url, response => {
+			if(response != "404"){
+				for(var obj in this.listDetailsDetails[title]){
+					if(this.listDetailsDetails[title][obj].url == url){
+						this.listDetailsDetails[title][obj].urlTitle = response[response.principaleAttributeName];
+						break;
+					}
+				}
+			}else{
+				this.listDetailsDetails[title][obj].urlTitle = "No Data";
+				this.listDetailsDetails[title][obj].url = "";
+			}
+		});
 	}
 }
